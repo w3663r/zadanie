530338f385b25b75d14aecfe95b8a1cdae8b9a7d
w3663r <47596792+w3663r@users.noreply.github.com>
2019-05-19 23:19:13 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,20 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from spis_tel import views
 from spis_tel import models
-#from rest_framework import routers, serializers, viewsets
-#from .serializers import OsobaViewSet, TelefonViewSet,EmailViewSet
-
-'''router = routers.DefaultRouter()
-router.register('osoba', OsobaViewSet)
-router.register('telefon', TelefonViewSet)
-router.register('email', EmailViewSet)
-'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('asd/', include(router.urls)),
     path('', views.index,name='list'),
-    #path('search', views.search,name='search'),
     path('add_osoba',views.CreateOsoba.as_view(), name='add_osoba'),
     path('add_telefon/<pk>',views.CreateTelefon.as_view(), name='add_telefon'),
     path('add_email/<pk>',views.CreateEmail.as_view(), name='add_email'),
