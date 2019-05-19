6d38603cc2528fa2c7363db5694e074e7c94ca83
w3663r <47596792+w3663r@users.noreply.github.com>
2019-05-19 23:30:10 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -12,4 +12,10 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if sys.argv[1] == 'doit':
+        os.system('pip3 install -r requirements.txt')
+        os.system('python3 manage.py makemigrations --empty spis_tel')
+        os.system('python3 manage.py loaddata fixture.json')
+        os.system('python3 manage.py migrate')
+        os.system('python3 manage.py runserver')
     execute_from_command_line(sys.argv)
