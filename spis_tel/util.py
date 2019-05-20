dea55c1f9fd5146d021e502ef1d15cd185d085bd
w3663r <47596792+w3663r@users.noreply.github.com>
2019-05-20 02:13:08 +0200

    Update util.py

--- spis_tel/util.py
+++ spis_tel/util.py
@@ -8,36 +8,22 @@ def get_data_list():
     os_obj = models.Osoba.objects
     tel_obj = models.Telefon.objects
     em_obj = models.Email.objects
-    l = [{'osoba':o, 'telefony':[t for t in tel_obj.filter(osoba_id= o.id)], 'emaile':[e for e in em_obj.filter(osoba_id= o.id)]} for o in os_obj.all()]
-    return l
+    return ([{'osoba':o, 'telefony':list(tel_obj.filter(osoba_id=o.id)), 'emaile':list(em_obj.filter(osoba_id=o.id))} for o in os_obj.all()])
+    
 
 def search(query):
-	
+	'''lista wyników wyszukiwania, każdy jak słownik w get_data_list'''
 	l = get_data_list()
-	if query:
-		if len(query.split(' ')) > 1 and query.strip():
+	if query and query.strip():
+		if len(query.split(' ')) > 1:
 			list_query = query.split(' ')
 			osoba_qset = (Q(imie__icontains=list_query[0])&Q(nazwisko__icontains=list_query[1])|Q(imie__icontains=list_query[1])&Q(nazwisko__icontains=list_query[0]))
 		else : osoba_qset = (Q(imie__icontains=query)|Q(nazwisko__icontains=query))
 		osoby = models.Osoba.objects.filter(osoba_qset).distinct()
 		telefony = models.Telefon.objects.filter(Q(telefon__icontains=query)).distinct()
 		emaile = models.Email.objects.filter(Q(email__icontains=query)).distinct()
-		results = []
-		# match search results with get_data_list list of dictionaries, add to results, avoid duplicates
-		#first approach:
-		'''if osoby:
-			lista_osob = [o.id for o in osoby]
-			results += [i for i in l if i['osoba'].id in lista_osob and i not in results] 
-		if telefony: 
-			lista_osob = [o.osoba_id for o in telefony]
-			results += [i for i in l if i['osoba'].id in lista_osob and i not in results] 
-		if emaile:
-			lista_osob = [o.osoba_id for o in emaile]
-			results += [i for i in l if i['osoba'].id in lista_osob and i not in results] '''
-		#second approach:
+
 		results = [i for i in l if i['osoba'] in osoby or set(i['telefony']).intersection(set(telefony)) or set(i['emaile']).intersection(set(emaile))]
 
 	else: results = []
 	return results
-
-
