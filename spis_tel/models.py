f299b305de3204ff6b433b99f4d44b0026a07694
w3663r <47596792+w3663r@users.noreply.github.com>
2019-05-20 02:35:33 +0200

    Update models.py

--- spis_tel/models.py
+++ spis_tel/models.py
@@ -1,29 +1,5 @@
 from django.db import models
 
-
-class OsobaManager(models.Manager):
-    '''nie wykorzystywane, zwraca listę obiektów typu Osoba z atrybutem telefon (lista telefonow w postaci str)'''
-
-    def with_info(self):
-        from django.db import connection
-        results = []
-        with connection.cursor() as cursor:
-            for row in cursor.execute("""
-                SELECT p.id, p.imie, p.nazwisko, t.telefon
-                FROM spis_tel_osoba p,spis_tel_telefon t
-                WHERE p.id = t.osoba_id 
-		ORDER BY p.id"""):
-                p = self.model(id=row[0], imie=row[1], nazwisko=row[2])
-                if p in results:
-                    results[results.index(p)].telefon.append(row[3])
-                else:
-                    p.telefon=[row[3]]
-                    results.append(p)
-                print(row)
-        print(results)
-        return results
-
-           
 class Osoba(models.Model):
 
 	imie = models.CharField(max_length=50)
