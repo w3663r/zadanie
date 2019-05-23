0eb92ef6ed0145708cee099592c9d1b7df5dd4fa
w3663r <47596792+w3663r@users.noreply.github.com>
2019-05-23 02:16:45 +0200

    Update util.py

--- spis_tel/util.py
+++ spis_tel/util.py
@@ -6,7 +6,7 @@ def search(query):
 	if query and query.strip():
 		list_query = query.split(' ')
 		if len(list_query) > 1:
-			q = (Q(imie__icontains=list_query[0])&Q(nazwisko__icontains=list_query[1])|Q(imie__icontains=list_query[1])&Q(nazwisko__icont4ains=list_query[0]))
+			q = (Q(imie__icontains=list_query[0])&Q(nazwisko__icontains=list_query[1])|Q(imie__icontains=list_query[1])&Q(nazwisko__icontains=list_query[0]))
 		else : q = Q(imie__icontains=query)|Q(nazwisko__icontains=query)|Q(telefony__telefon__icontains=query)|Q(emaile__email__icontains=query)
 		osoby = models.Osoba.objects.filter(q).distinct()
 		return osoby
